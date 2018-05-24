f3f6422c75ca9cb9d7b0b6c920b3a86ad312c340
yeonduru <durury20@gmail.com>
2018-05-25 03:59:49 +0900

    Revert "Rating model added to models.py"

--- cvs_server/cvs_server/settings.py
+++ cvs_server/cvs_server/settings.py
@@ -41,10 +41,6 @@ INSTALLED_APPS = [
     'cvs_rest.apps.CvsRestConfig',
 ]
 
-REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
-}
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- cvs_server/cvs_server/urls.py
+++ cvs_server/cvs_server/urls.py
@@ -20,6 +20,5 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^', include('cvs_rest.urls')),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
