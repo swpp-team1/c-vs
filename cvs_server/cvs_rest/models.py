adf60a33ac113cd07c0cf0e93e22420e03b649d4
YEONJU LEE <durury20@gmail.com>
2018-06-02 00:47:26 +0900

    added edited to Rating model and modified some fields to be necessary

--- cvs_server/cvs_rest/models.py
+++ cvs_server/cvs_rest/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import fields
 
+class CustomUser(AbstractUser):
+    created = models.DateTimeField(auto_now_add=True)
+    nickname = models.CharField(max_length=20, unique=True)
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
@@ -29,14 +32,12 @@ class Product(models.Model):
     class Meta:
         ordering = ('manufacturer', 'name',)
 
-class CustomUser(AbstractUser):
-    created = models.DateTimeField(auto_now_add=True)
-    nickname = models.CharField(max_length=20, unique=True)
 
 class Recipe(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     edited = models.DateTimeField(auto_now=True)
-    title = models.CharField(max_length=100) 
+    title = models.CharField(max_length=100, null=False)
+    #ingredients = models.TextField()
     ingredients = models.ManyToManyField('Product')
     user_id = models.ForeignKey(
         'CustomUser',
@@ -50,25 +51,12 @@ class Recipe(models.Model):
     class Meta:
         ordering = ('created',)
 
-class Rating(models.Model) :
-    created = models.DateTimeField(auto_now_add=True)
-    value = models.PositiveSmallIntegerField()
-    user_id = models.ForeignKey(
-        'CustomUser',
-        on_delete=models.CASCADE,
-    )
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    belong_to = fields.GenericForeignKey('content_type', 'object_id')
-
-    class Meta:
-        ordering = ('created',)
-
 
 class Review(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     edited = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=100)
+    #rating = models.PositiveIntegerField()
     user_id = models.ForeignKey(
         'CustomUser',
         on_delete=models.CASCADE,
@@ -76,19 +64,19 @@ class Review(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     belong_to = fields.GenericForeignKey('content_type', 'object_id') 
+    ratings = fields.GenericRelation('Rating', related_query_name='comments')
     
     posts = fields.GenericRelation('Post', related_query_name='reviews')    
-    ratings = fields.GenericRelation('Rating', related_query_name='reviews')
+    
     
     class Meta:
         ordering = ('created',)
-
 
 class Comment(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     edited = models.DateTimeField(auto_now=True)
     content = models.TextField()
-    rating = fields.GenericRelation('Rating', related_query_name='comments')
+    #rating = models.PositiveIntegerField()
     user_id = models.ForeignKey(
         'CustomUser',
         on_delete=models.CASCADE,
@@ -111,5 +99,23 @@ class Post(models.Model):
     object_id = models.PositiveIntegerField()
     belong_to = fields.GenericForeignKey('content_type', 'object_id')
     
+    class Meta:
+        ordering = ('created',)
+
+
+class Rating(models.Model) :
+    created = models.DateTimeField(auto_now_add=True)
+    edited = models.DateTimeField(auto_now=True)
+    value = models.PositiveSmallIntegerField(null=False)
+    
+    user_id = models.ForeignKey(
+        'CustomUser',
+        on_delete=models.CASCADE,
+    )
+    
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    belong_to = fields.GenericForeignKey('content_type', 'object_id')
+
     class Meta:
         ordering = ('created',)
