25009071987c7aa64c3481d65dfafda6702f5b9f
9Hyeonwoo <koo999@wafflestudio.com>
2018-06-06 11:31:19 +0900

    Add pagination 30 per page

--- cvs_server/config/settings/base.py
+++ cvs_server/config/settings/base.py
@@ -150,8 +150,8 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
     # for Pagination
-    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    # 'PAGE_SIZE': 100
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 30,
 }
 
 # Setting for Image path
