cd85f21711989134c3e4eec4c94bc5e4e2ad5148
yeonduru <durury20@gmail.com>
2018-06-09 19:15:14 +0900

    Implemented PUT for Comment model (#45)
    * Comment PUT implemented
    
    * added try except statement on getting Rating object
    
    * Review PUT Implemented
    
    * implemented Post GET ALL/POST VIEW (/posts/) with image upload
    
    * implemented Review POST (/reviews/)
    
    * implemented Review PUT (/reviews/pk/)
    
    * added unfilled http status in all Responses
    
    * added profile image and profile content to Review views and implemented /posts/pk/ view for review PUT(modifying) function
    
    * made UserIdSerializer to provide shortened informations of user in Comment and Review view
    
    * correct typo
    
    * Implemented Recipe view
    
    * .

--- cvs_server/cvs_rest/serializers.py
+++ cvs_server/cvs_rest/serializers.py
@@ -2,43 +2,33 @@ from rest_framework import serializers
 from cvs_rest.models import *
 from django.db.models import Avg
 
+DEFAULT_IMAGE = "default.png"
+
 class UserSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = CustomUser
         fields = ('id', 'username', 'created', 'email', 'comment_set', 'review_set')
-        
 
-class RecipeSerializer(serializers.ModelSerializer) :
-    class Meta:
-        model = Recipe
-        fields = '__all__'
+#serializes only pk and username
+class UserIdSerializer(serializers.ModelSerializer) :
+    
+    class Meta :
+        model = CustomUser
+        fields = ('id', 'username')
+        depth = 2
 
-class RatingSerializer(serializers.ModelSerializer) :
-
-    #override
-    def create(self, validated_data) :
-        return Rating.objects.create(**validated_data)
-    
-    #override
-    def update(self, instance, validated_data) :
-        instance.edited = validated_data.get('edited', instance.edited)
-        instance.value = validated_data.get('value', instance.value)
-        instance.save()
-        return instance
-    
-    
-    class Meta:
-        model = Rating
-        fields = ('id', 'created', 'edited', 'value', 'user_id', 'comment')
 
 class CommentSerializer(serializers.ModelSerializer) :
 
     rating = serializers.SerializerMethodField()
+    user_id = UserIdSerializer()
 
     class Meta:
         model = Comment
         fields = ('id', 'created', 'edited', 'content', 'user_id', 'product', 'rating')
+        depth = 1
+    
     
     def get_rating(self, obj):
         rat = obj.rating.get()
@@ -66,36 +56,148 @@ class ProductSerializer(serializers.ModelSerializer) :
 
 
 class PostSerializer(serializers.ModelSerializer) :
+
+    image = serializers.ImageField()
+
     class Meta :
         model = Post
         fields = ('created', 'image', 'content')
+        depth = 2
 
 class ReviewListSerializer(serializers.ModelSerializer) :
 
+    user_id = UserIdSerializer()
     rating = serializers.SerializerMethodField()
+    
+    profile_image = serializers.SerializerMethodField()
+    profile_content = serializers.SerializerMethodField()
 
     class Meta:
        model = Review
-       fields = ('id', 'created', 'edited', 'title', 'user_id', 'product', 'rating')
+       fields = ('id', 'created', 'edited', 'title', 'user_id', 'profile_image', 'profile_content', 'product', 'rating')
+       depth = 1
     
     def get_rating(self, obj) :
         rat = obj.rating.get()
         if rat:
             return rat.value
         return 0
+    
+    def get_profile_image(self, obj) :
+        review_type = ContentType.objects.get_for_model(Review)
+        posts = Post.objects.filter(content_type__pk=review_type.id, object_id=obj.id)
+        for post in posts :
+            if post.image != DEFAULT_IMAGE :
+                return "/media/" + str(post.image)
+        return "/media/" + DEFAULT_IMAGE
+    
+    def get_profile_content(self, obj) :
+        review_type = ContentType.objects.get_for_model(Review)
+        posts = Post.objects.filter(content_type__pk=review_type.id, object_id=obj.id)
+        for post in posts :
+            if post.content :
+                return post.content[:50]
+        return "No preview content"
 
 class ReviewDetailSerializer(serializers.ModelSerializer) :
 
+    user_id = UserIdSerializer()
     rating = serializers.SerializerMethodField()
     post = PostSerializer(many=True)
 
+    profile_image = serializers.SerializerMethodField()
+
     class Meta :
         model = Review
-        fields = ('created', 'edited', 'title', 'user_id', 'product', 'post', 'rating')
+        fields = ('id', 'created', 'edited', 'title', 'user_id', 'profile_image', 'product', 'post', 'rating')
+        depth = 1
     
     def get_rating(self, obj) :
         rat = obj.rating.get()
         if rat :
             return rat.value
         return 0
+    
+    def get_profile_image(self, obj) :
+        review_type = ContentType.objects.get_for_model(Review)
+        posts = Post.objects.filter(content_type__pk=review_type.id, object_id=obj.id)
+        for post in posts :
+            if post.image != DEFAULT_IMAGE :
+                return "/media/" + str(post.image)
+        return "/media/" + DEFAULT_IMAGE
 
+
+class RecipeListSerializer(serializers.ModelSerializer) :
+    user_id = UserIdSerializer()
+
+    profile_image = serializers.SerializerMethodField()
+    profile_content = serializers.SerializerMethodField()
+    price_all = serializers.SerializerMethodField()
+
+    class Meta :
+        model = Recipe
+        fields = ('id', 'created', 'edited', 'title', 'user_id', 'profile_image', 'profile_content', 'price_all','ingredients')
+        depth = 1
+    
+    def get_profile_image(self, obj) :
+        recipe_type = ContentType.objects.get_for_model(Recipe)
+        posts = Post.objects.filter(content_type__pk=recipe_type.id, object_id=obj.id)
+        for post in posts :
+            if post.image != DEFAULT_IMAGE :
+                return "/media/" + str(post.image)
+        return "/media/" + DEFAULT_IMAGE
+    
+    def get_profile_content(self, obj) :
+        recipe_type = ContentType.objects.get_for_model(Recipe)
+        posts = Post.objects.filter(content_type__pk=recipe_type.id, object_id=obj.id)
+        for post in posts :
+            if post.content :
+                return post.content[:50]
+        return "No preview content"
+
+    def get_price_all(self, obj) :
+        ingredients = obj.ingredients.all()
+        sum = 0
+        for ingre in ingredients :
+            sum += int(ingre.price)
+        return sum
+
+class RecipeDetailSerializer(serializers.ModelSerializer) :
+
+    user_id = UserIdSerializer()
+    post = PostSerializer(many=True)
+
+    profile_image = serializers.SerializerMethodField()
+    price_all = serializers.SerializerMethodField()
+
+    class Meta :
+        model = Recipe
+        fields = ('id', 'created', 'edited', 'title', 'user_id', 'profile_image', 'price_all', 'ingredients', 'post')
+        depth = 1
+    
+    def get_profile_image(self, obj) :
+        recipe_type = ContentType.objects.get_for_model(Recipe)
+        posts = Post.objects.filter(content_type__pk=recipe_type.id, object_id=obj.id)
+        for post in posts :
+            if post.image != DEFAULT_IMAGE :
+                return "/media/" + str(post.image)
+        return "/media/" + DEFAULT_IMAGE
+    
+    def get_price_all(self, obj) :
+        ingredients = obj.ingredients.all()
+        sum = 0
+        for ingre in ingredients :
+            sum += int(ingre.price)
+        return sum
+
+
+
+#this serializer is used only for testing
+class RatingSerializer(serializers.ModelSerializer) :
+
+    user_id = UserIdSerializer()
+
+    class Meta:
+        model = Rating
+        fields = ('id', 'created', 'edited', 'value', 'user_id', 'product', 'content_type', 'object_id')
+        depth = 2
